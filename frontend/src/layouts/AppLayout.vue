df9dd0e241f9c24cd2ee0115f5e2284f8f4add6f
golovanya <golovanya1507@.com>
2023-09-26 21:35:50 +0300

    task2_1

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app_layout">
+      <app-layout-header />
+    </div>
+  </template>
+  
+  <script setup>
+  import AppLayoutHeader from "./AppHeader.vue";
+  </script>
+  
+  <style lang="scss" scoped>
+  .app_layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  </style>
