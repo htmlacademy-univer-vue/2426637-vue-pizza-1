7604625a79c63661b8b76056dc438eae73e84240
Golovanya <121174635+Golovanya@users.noreply.github.com>
2024-01-04 13:01:52 +0300

    fix-bags

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,50 @@
 <template>
   <app-layout>
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </app-layout>
 </template>
 
 <script setup>
-import { AppLayout } from "@/layouts";
-import { useDataStore, useAuthStore } from "./stores";
+import AppLayout from "@/layouts/AppLayout.vue";
+import { onMounted, ref } from "vue";
+import { useDataStore } from "@/stores/data";
+import { useAuthStore } from "@/stores/auth";
+import JwtService from "@/services/jwt/jwt.service";
+import  router  from "@/router";
+import { useRoute } from "vue-router";
 
 const dataStore = useDataStore();
-const authStore = useAuthStore();
+const route = useRoute();
+const isLoaded = ref(false);
 
-dataStore.fetchdata();
+const checkLoggedIn = async () => {
+  const authStore = useAuthStore();
+  const token = JwtService.getToken();
+  if (!token) {
+    isLoaded.value = true;
+    return;
+  }
 
-authStore.whoAmI();
+  try {
+    await authStore.whoami();
+    const { redirect } = route.query;
+    await router.push(redirect ? redirect : { name: "home" });
+  } catch (e) {
+    JwtService.destroyToken();
+    console.error(e);
+  } finally {
+    isLoaded.value = true;
+  }
+};
 
+onMounted(() => {
+  checkLoggedIn();
+  dataStore.loadData();
+});
 </script>
 
 <style lang="scss">
